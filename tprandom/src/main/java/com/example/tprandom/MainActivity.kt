2519d84be843fabeaddf6fb362e17b740b98e3fd
hherve2023 <helene.mazgaldi@gmail.com>
2023-06-21 12:21:32 +0200

    mod 4 +tp

--- tprandom/src/main/java/com/example/tprandom/MainActivity.kt
+++ tprandom/src/main/java/com/example/tprandom/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.tprandom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
